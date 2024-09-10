00cc1a7a7fc179030776281f24b05ca100a31b6e
Paul Laulan <paul@growthweb3.com>
2024-09-10 14:45:50 +0200

    add new files

---
+++ deploy/deploy-factory.ts
@@ -0,0 +1,25 @@
+import { utils, Wallet } from "zksync-ethers";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+  // Private key of the account used to deploy
+  const wallet = new Wallet("6e0fc49e35ea0228f025e15ebf37be320e919a14b514f38c6cc67cd8e3742d58");
+  const deployer = new Deployer(hre, wallet);
+  const factoryArtifact = await deployer.loadArtifact("AAFactory");
+  const aaArtifact = await deployer.loadArtifact("TwoUserMultisig");
+
+  // Getting the bytecodeHash of the account
+  const bytecodeHash = utils.hashBytecode(aaArtifact.bytecode);
+
+  const factory = await deployer.deploy(factoryArtifact, [bytecodeHash], undefined, [
+    // Since the factory requires the code of the multisig to be available,
+    // we should pass it here as well.
+    aaArtifact.bytecode,
+  ]);
+
+  const factoryAddress = await factory.getAddress();
+
+  console.log(`AA factory address: ${factoryAddress}`);
+}
